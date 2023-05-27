bf6aded1b34b3738aabbd49147913a0dc0e2a9e0
codingNoob12 <crm26@naver.com>
2023-05-27 18:27:00 +0900

    AOP로 성능 측정 구현 (Learn Spring AOP)

---
+++ learn-spring-aop/src/main/java/com/in28minutes/learnspringaop/aopexample/aspects/PerformanceTrackingAspect.java
@@ -0,0 +1,35 @@
+package com.in28minutes.learnspringaop.aopexample.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class PerformanceTrackingAspect {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	// JoinPoint는 Advice의 객체 인스턴스이지만 실행에는 관여하지 못함 => ProceedingJoinPoint
+	@Around("execution(* com.in28minutes.learnspringaop.aopexample.*.*.*(..))")
+	public Object findExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		//Starter a timer
+		long startTimeMillis = System.currentTimeMillis();
+		
+		//Execute the method
+		Object returnValue = proceedingJoinPoint.proceed(); //리턴값을 받아주지 않으면 NullPointException 발
+		
+		//Stop the timer
+		long stopTimeMillis = System.currentTimeMillis();
+		
+		long executionDuration = stopTimeMillis - startTimeMillis;
+		
+		logger.info("Around Aspect - {} Method executed in {} ms", 
+				proceedingJoinPoint, executionDuration);
+		
+		return returnValue;
+	}
+}
